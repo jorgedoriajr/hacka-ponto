5f867a0cc83f297453464761f4d1746330950b7e
GabrielAranda1 <per.gabrielaranda@gmail.com>
2024-03-23 13:37:16 -0300

    fix: auth issues

--- src/presentation/gateway/middlewares/Authentication.ts
+++ src/presentation/gateway/middlewares/Authentication.ts
@@ -2,6 +2,10 @@ import { Request, Response, NextFunction } from 'express'
 import { JwtAuthenticationService } from '../../../infra/security/JwtAuthenticationService'
 export function checkAuthentication(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers
+  let { employeeId } = req.params
+
+  if (!employeeId)
+    employeeId = req.body.employeeId
 
   if (!authorization) {
     return res.status(401).json({ error: 'Unauthorized' })
