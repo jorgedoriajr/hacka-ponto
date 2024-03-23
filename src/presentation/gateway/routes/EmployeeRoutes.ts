041341f14cb0cbe62364811a34ecae82b74a5694
GabrielAranda1 <per.gabrielaranda@gmail.com>
2024-03-23 11:42:06 -0300

    feat: added authentication to register time entry endpoint

--- src/presentation/gateway/routes/EmployeeRoutes.ts
+++ src/presentation/gateway/routes/EmployeeRoutes.ts
@@ -11,7 +11,7 @@ import { checkAuthentication } from '../middlewares/Authentication'
 function registerEmployeeRoutes(router: Router) {
   router.post('/employees', adaptRoute(container.resolve(CreateEmployeeController)))
   router.post('/employees/:employeeId/generate-month-report', checkAuthentication, adaptRoute(container.resolve(SendMonthlyReportToEmployeeController)))
-  router.post('/journey', adaptRoute(container.resolve(RegisterTimeEntryController)))
+  router.post('/journey', checkAuthentication, adaptRoute(container.resolve(RegisterTimeEntryController)))
   router.get('/journey/:employeeId', checkAuthentication, adaptRoute(container.resolve(GetDaWorkJourneyController)))
   router.post('/login', adaptRoute(container.resolve(SignInEmployeeController)))
 
